78316fde4444ba5e78ebd52255a6bb0ddac35197
S8INT <petrkateeb4@gmail.com>
2022-12-14 01:38:12 -0800

    101-natural

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,16 +25,16 @@ _putchar(' ');
 _putchar(' ');       
  _putchar((res % 10) + '0');
 }
-else if (res >= 10 && res < 100)     
+else if (res >= 10 && res < 100)
 {
-_putchar(' '); 
+_putchar(' ');
 _putchar((res / 10) + '0');
-_putchar((res % 10) + '0');       
-}   
-else if (res >= 100 && j != 0)    
-{       
-_putchar((res / 100) + '0');       
-_putchar((res / 10) % 10 + '0');       
+_putchar((res % 10) + '0');
+}
+else if (res >= 100 && j != 0)
+{
+_putchar((res / 100) + '0');
+ _putchar((res / 10) % 10 + '0');
 _putchar((res % 10) + '0');
 }
 else
