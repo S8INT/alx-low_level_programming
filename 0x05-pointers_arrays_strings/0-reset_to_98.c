026ba3473d90d3d6f3b01dd9dacda32725c3764f
S8INT <petrkateeb4@gmail.com>
2022-12-19 06:35:25 -0800

    str_length

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,9 +3,9 @@
  *main - function takes a pointer to an int n
  * and updates the value it points to
  *Return: Always 0
- */
+*/
 
 void reset_to_98(int *n)
 {
-  *n = 98;
+*n = 98;
 }
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * puts - Write a function that prints a string, followed by a new line, to
+ * stdout.
+ *
+ * @str: This is my input string
+ *
+*/
+void _puts(void *str)
+{
+int len;
+for (len = 0: str[len] != '\0'; len++)
+{
+_putchar(str[len]);
+}
+_putchar('\n');
+}
