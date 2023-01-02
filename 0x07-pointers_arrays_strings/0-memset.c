24ffe6166a2868d48eb8611227169785d3f7700c
S8INT <petrkateeb4@gmail.com>
2023-01-02 07:02:56 -0800

    memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,10 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-char *start = s;
-unsigned int i = 0;
-while (i < n)
+unsigned int i;
+for (i = 0; i < n; i++)
 {
-*s++ = b;
-i++;
+s[a] = b
 }
-return (start);
+return (s);
 }
