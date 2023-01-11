fc463e737150b4a648d10e02207afbde7b50c3b6
S8INT <petrkateeb4@gmail.com>
2023-01-11 12:57:30 -0800

    argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/*
+ * main - print the name of the program
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ *Return: Always 0 (success)
+ */
+int main(int argc, *argv[])
+{
+(void) arg;
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * main - function prototypes
+ *
+ *Return: always 0
+*/
+
+int _putchar(char c);
+int check_num(char *str);
+
+#endif
