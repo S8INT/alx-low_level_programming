dd09e09c0e3122ecbee23d0392e5c0337474146e
S8INT <petrkateeb4@gmail.com>
2023-01-16 08:03:03 -0800

    update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /*
  * main - prints the name of the file program was compiled from
  *
@@ -6,6 +7,6 @@
 */
 int main(void)
 {
-printf("%s\n", _BASE_FILE_);
+printf("%s\n", _FILE_);
 return (0);
 }
