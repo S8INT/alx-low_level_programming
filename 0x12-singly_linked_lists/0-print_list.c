251a989542e7d6aaa407ddff15577ad4851bd65b
S8INT <petrkateeb4@gmail.com>
2023-01-26 09:13:44 -0800

    print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/*
+ * print_list - print the contents of a list.
+ * @h: pointer to list.
+ *
+ * Return: number of elements in a list
+*/
+
+size_t print_list(const list_t *h)
+{
+size_t i;
+for (i = 0; h; i++)
+{
+printf("%d\n", h->n);
+h = h->next;
+}
+return (i);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+you are expected to be able to explain to anyone, When and why using linked lists vs arrays, and How to build and use linked lists
