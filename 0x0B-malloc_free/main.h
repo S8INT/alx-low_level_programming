e51e64f4b9e244e54d73691eeb1c8ea09c00cf2d
S8INT <petrkateeb4@gmail.com>
2023-01-29 02:01:38 -0800

    strdup.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-_putchar(char c);
+#include <stdio.h>
+#include <stdlib.h>
+int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
