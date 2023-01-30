617e6b6e1d4f7d6187705bf4c2516842c9c0adec
S8INT <petrkateeb4@gmail.com>
2023-01-30 00:06:17 -0800

    update

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,8 +4,8 @@
  * @s: input
  * Return: Always 0 (Success)
  */
-void _print_rev_recursion(char *s) 
-{  
+void _print_rev_recursion(char *s)
+{
 if (*s != '\0')
 {
 _print_rev_recursion(s + 1);
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,11 +4,11 @@
  * @s: input
  * Return: Always 0 (Success)
  */
-int _strlen_recursion(char *s)  
+int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 {
-return (1 + _strlen_recursion(s + 1)); 
+return (1 + _strlen_recursion(s + 1));
 }
 return (0);
 }
