190034c6907e1ba8061b73eabc91c5763dab1a3a
S8INT <petrkateeb4@gmail.com>
2023-03-24 01:51:18 -0700

    update

--- 0x10-variadic_functions/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _puchar(char c)
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
-
-#endif/*main_h*/
