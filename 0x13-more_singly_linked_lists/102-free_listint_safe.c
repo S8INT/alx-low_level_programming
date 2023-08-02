9bfa6ca6aa9359f6d0bed594f8168dca65b938f0
PETER <petrkateeb4@gmail.com>
2023-08-02 13:36:42 -0700

    update

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,19 +8,18 @@
  */
 void free_listp2(listp_t **head)
 {
-	listp_t *temp;
-	listp_t *curr;
-
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
+listp_t *temp;
+listp_t *curr;
+if (head != NULL)
+{
+curr = *head;
+while ((temp = curr) != NULL)
+{
+curr = curr->next;
+free(temp);
+}
+*head = NULL;
+}
 }
 
 /**
@@ -31,42 +30,36 @@ void free_listp2(listp_t **head)
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t nnodes = 0;
-	listp_t *hptr, *new, *add;
-	listint_t *curr;
+size_t nnodes = 0;
+listp_t *hptr, *new, *add;
+listint_t *curr;
+hptr = NULL;
+while (*h != NULL)
+{
+new = malloc(sizeof(listp_t));
+if (new == NULL)
+exit(98);
+new->p = (void *)*h;
+new->next = hptr;
+hptr = new;
+add = hptr;
+while (add->next != NULL)
+{
+add = add->next;
+if (*h == add->p)
+{
+*h = NULL;
+free_listp2(&hptr);
+return (nnodes);
+}
+}
+curr = *h;
+*h = (*h)->next;
+free(curr);
+nnodes++;
+}
 
-	hptr = NULL;
-	while (*h != NULL)
-	{
-		new = malloc(sizeof(listp_t));
-
-		if (new == NULL)
-			exit(98);
-
-		new->p = (void *)*h;
-		new->next = hptr;
-		hptr = new;
-
-		add = hptr;
-
-		while (add->next != NULL)
-		{
-			add = add->next;
-			if (*h == add->p)
-			{
-				*h = NULL;
-				free_listp2(&hptr);
-				return (nnodes);
-			}
-		}
-
-		curr = *h;
-		*h = (*h)->next;
-		free(curr);
-		nnodes++;
-	}
-
-	*h = NULL;
-	free_listp2(&hptr);
-	return (nnodes);
+*h = NULL;
+free_listp2(&hptr);
+return (nnodes);
 }
