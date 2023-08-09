aedce9b4888e76dc2f05e2a07b4ddcb29fa7ca13
PETER <petrkateeb4@gmail.com>
2023-08-09 06:58:18 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,20 +10,28 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int fd;
-  ssize_t rd, Nwr;
-  char *buf;
-  if (!filename)
-    return(0);
-  fd = open(filename, O_RDONLY);
-  if(fd == -1)
-    return (0);
-  buf = malloc(sizeof(char) *(letters));
-  if (!buf)
-    return (0);
-  rd = read(fd, buf, letters);
-  Nwr = write(STDOUT_FILENO, buf, rd);
-  close (fd);
-  free (buf);
-  return (Nwr);
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
 }
